27bb57179c886657af3e76d5270fda7c41c2d084
xp12-mcmin <2249528587@qq.com>
2026-06-04 23:42:51 +0800

    添加双端口签到程序

---
+++ 一键签到.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo Starting port 8765...
+start cmd /k "python "C:\Users\xp123\Desktop\qai\Ç©µ½.py""
+echo Starting port 5678...
+start cmd /k "python "C:\Users\xp123\Desktop\qai\Ç©µ½5678.py""
+echo Done
+pause
